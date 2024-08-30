646870a25754018b2f96923a7a9220d4dcb0ba59
ana luisa <113107603+luisadiniz15@users.noreply.github.com>
2024-08-30 11:36:43 -0300

    script

---
+++ script.js
@@ -0,0 +1,47 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaAlternativas= document.querySelector(".caixa-alternativas");
+const caixaResultado= document.querySelector("caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
+
+
+const perguntas = [
+    {
+        enunciado: "Quais sãos as principais causas da queimada?",
+        alternativas: [
+            "Na maioria das vezes, essas queimadas são provocadas pela ação humana de maneira criminosa."
+            "O calor do sol."
+        ]
+    }
+    {
+        enunciado: "Como as queimadas afetam uma cidade?",
+        alternativas: [
+            "Não afeta em nada"
+            "Destruir áreas produtivas como lavouras e pastagens."
+        ]
+    }
+    {
+        enunciado: "Quais são algumas medidas eficazes de prevenir as queimadas?",
+        alternativas: [
+            "Extinção Responsável de Fogueiras e Fontes de Calor"
+            "Deixar fogueiras acesas"
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+function mostraAlternativas (){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas= document.createElement("button");
+        botaoAlternativas.textContent= alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
+mostraPerguta()
