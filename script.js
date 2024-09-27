04cbe75c6dd8090a4239db427fc24b61d81c6a8c
ana luisa <113107603+luisadiniz15@users.noreply.github.com>
2024-09-27 11:16:31 -0300

    js

--- script.js
+++ script.js
@@ -54,6 +54,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal="";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -65,12 +66,16 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
       const botaoAlternativas = document.createElement("button");
       botaoAlternativas.textContent = alternativa.texto;
-      botaoAlternativas.addEventListener("click", function () {
-        atual++;
-        mostraPergunta();
-      });
+      botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
       caixaAlternativas.appendChild(botaoAlternativas);
-    }
+        }
+      });
+      function respostaSelecionada(opcaoSelecionada){
+        const afirmacoes = opcaoSelecionada.afirmacoes;
+        historiaFinal=afirmacoes;
+        atual++;
+        mostraPergunta ();
+    
   }
   
 mostraPergunta();
